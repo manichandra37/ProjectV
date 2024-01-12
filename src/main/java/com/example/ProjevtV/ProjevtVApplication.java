e80b8520944bddfc6580acbe6e3b733ac4ad7a64
Manichandra <manichandramaddi73@gmail.com>
2024-01-12 03:26:51 -0600

    Initial commit

---
+++ src/main/java/com/example/ProjevtV/ProjevtVApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjevtV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjevtVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjevtVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjevtV/ProjevtVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjevtV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjevtVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

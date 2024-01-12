3dad00b34b5d7ff8232a73eff23cac8f8757b6a4
Manichandra <manichandramaddi73@gmail.com>
2024-01-12 03:31:21 -0600

    Tesing the initial push

--- src/main/java/com/example/ProjevtV/ProjevtVApplication.java
+++ src/main/java/com/example/ProjevtV/ProjevtVApplication.java
@@ -8,6 +8,7 @@ public class ProjevtVApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjevtVApplication.class, args);
+		// Testing the push functionality from eclispe
 	}
 
 }

c15463127fab977e7f765bf1a8090f05c21e16fd
Manichandra <manichandramaddi73@gmail.com>
2024-01-15 02:32:11 -0600

    Modified name of few classes

--- src/main/java/com/vivek/controller/VivekController.java
+++ src/main/java/com/vivek/controller/VivekController.java
@@ -1,13 +1,13 @@
-package com.example.ProjevtV;
+package com.vivek.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjevtVApplication {
+public class VivekController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjevtVApplication.class, args);
+		SpringApplication.run(VivekController.class, args);
 		// Testing the push functionality from eclispe
 	}
 
